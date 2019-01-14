10c2ed60c39a5b18b6443cbed8278101c650a46a
Nicena Fontaine <nicenas.email@gmail.com>
2019-01-14 15:21:43 -0500

    Add function to be triggered when button is clicked.

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+// Global Variables
+
 var question;
-var magicEightBall = {
-};
+var button = document.getElementById('question-btn');
+
+// magicEightBall Object, properties, and methods
+
+var magicEightBall = {};
 magicEightBall.answerList = ["It is certain.", "As I see it, yes.", "Reply hazy, try again.", "Don't count on it.", "It is decidedly so.", "Most likely.", "Ask again later.", "My reply is no.", "Without a doubt.", "Outlook good.", "Better not tell you now.", "My sources say no.", "Yes - definitely.", "Yes.", "Cannot predict now.", "Outlook not so good.", "You may rely on it.", "Signs point to yes.", "Concentrate and ask again.", "Very doubtful."];
-magicEightBall.questionAnswer = function(question) {
+magicEightBall.provideAnswer = function(question) {
 	var randomNumber = Math.random();
 	var randomNumArray = randomNumber * this.answerList.length;
 	var randomIndex = Math.floor(randomNumArray);
@@ -10,4 +15,13 @@ magicEightBall.questionAnswer = function(question) {
 	console.log(question);
 	console.log(randomAnswer);
 };
-magicEightBall.questionAnswer("Will I ever learn JS?");
+
+var askQuestion = function () {
+	question = prompt('Ask me your question');
+	magicEightBall.provideAnswer(question);
+};
+
+//magicEightBall.questionAnswer("Will I ever learn JS?");
+
+// Event Listener
+button.addEventListener('click', askQuestion);
